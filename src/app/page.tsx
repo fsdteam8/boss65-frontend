df2ca8eedef46d3d0fe62a0f01e575b6e755efb8
ILhankhondaker <ilhankhondaker@gmail.com>
2025-05-17 16:10:05 +0600

    add showroom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import ShowRoom from "@/components/ShowRoom";
 
 export default function Home() {
   return (
   <div>
  <Hero/>
+ <ShowRoom/>
   </div>
   );
 }
