e057f58b38b03135235f69ff46e7df5981e5d29b
ILhankhondaker <ilhankhondaker@gmail.com>
2025-05-18 14:29:13 +0600

    add content

--- src/app/our-unique-experience/page.tsx
+++ src/app/our-unique-experience/page.tsx
@@ -1,11 +1,17 @@
+import Faq from '@/components/Faq'
 import HeroSection from '@/components/Hero'
 import { Experience, PrivateEscapeH1 } from '@/components/hero-title'
+import ReviewCarousel from '@/components/Review'
+import SocialChamber from '@/components/SocialChamber'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <HeroSection heading={<Experience />}/>
+      <SocialChamber/>
+      <Faq/>
+      <ReviewCarousel/>
     </div>
   )
 }
