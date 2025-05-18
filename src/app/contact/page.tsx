be7af7f8c6efbc5bbb8eac172fe0bd9d5b9197e6
ILhankhondaker <ilhankhondaker@gmail.com>
2025-05-18 16:10:25 +0600

    add review in contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/Hero'
 import { Contact } from '@/components/hero-title'
+import ReviewCarousel from '@/components/Review'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <HeroSection heading={<Contact/>}/>
+      <ReviewCarousel/>
     </div>
   )
 }
