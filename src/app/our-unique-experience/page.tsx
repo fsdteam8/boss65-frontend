10852715c6d70faeb3b59ff275ec37348a5bcf67
ILhankhondaker <ilhankhondaker@gmail.com>
2025-05-18 14:55:55 +0600

    add room gelarry

--- src/app/our-unique-experience/page.tsx
+++ src/app/our-unique-experience/page.tsx
@@ -2,6 +2,7 @@ import Faq from '@/components/Faq'
 import HeroSection from '@/components/Hero'
 import { Experience, PrivateEscapeH1 } from '@/components/hero-title'
 import ReviewCarousel from '@/components/Review'
+import RoomGallery from '@/components/RoomGallery'
 import SocialChamber from '@/components/SocialChamber'
 import React from 'react'
 
@@ -10,6 +11,7 @@ const page = () => {
     <div>
       <HeroSection heading={<Experience />}/>
       <SocialChamber/>
+      <RoomGallery/>
       <Faq/>
       <ReviewCarousel/>
     </div>
