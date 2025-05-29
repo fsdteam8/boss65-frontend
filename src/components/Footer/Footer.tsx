6ad3671aeca4a48b55bf019fd24b7a19fe785f17
ILhankhondaker <ilhankhondaker@gmail.com>
2025-05-29 13:09:45 +0600

    add fotter social link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Globe, Instagram } from "lucide-react";
+import {  Instagram } from "lucide-react";
 import Image from "next/image";
 
 export default function Footer() {
